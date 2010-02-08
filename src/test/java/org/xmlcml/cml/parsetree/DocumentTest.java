75540e878440c307d4819956fdea53008aabdd47
pm286 <733515+petermr@users.noreply.github.com>
2010-02-08 18:21:01 +0000

    fixedTest

--- src/test/java/org/xmlcml/cml/parsetree/DocumentTest.java
+++ src/test/java/org/xmlcml/cml/parsetree/DocumentTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.xmlcml.cml.base.CMLUtil;
 
@@ -26,31 +27,34 @@ import org.xmlcml.cml.base.CMLUtil;
  */
 public class DocumentTest {
 
-    public static String[] filenames = {
-        "src/test/resources/parsetrees/target/file1.xml",
-        "src/test/resources/parsetrees/target/file2.xml",
-        "src/test/resources/parsetrees/target/file3.xml",
-        "src/test/resources/parsetrees/target/file4.xml",
-        "src/test/resources/parsetrees/target/file5.xml",
-        "src/test/resources/parsetrees/target/file6.xml",
-        "src/test/resources/parsetrees/target/file7.xml",
-        "src/test/resources/parsetrees/target/file8.xml",
-        "src/test/resources/parsetrees/target/file9.xml",
-    };
+	String[] filenames = null;
+    POSDocument[] documents = null;
+	private void makeFiles() {
+		if (filenames == null) {
+			filenames = new String[] {
+	        "src/test/resources/parsetrees/target/file1.xml",
+	        "src/test/resources/parsetrees/target/file2.xml",
+	        "src/test/resources/parsetrees/target/file3.xml",
+	        "src/test/resources/parsetrees/target/file4.xml",
+	        "src/test/resources/parsetrees/target/file5.xml",
+	        "src/test/resources/parsetrees/target/file6.xml",
+	        "src/test/resources/parsetrees/target/file7.xml",
+	        "src/test/resources/parsetrees/target/file8.xml",
+	        "src/test/resources/parsetrees/target/file9.xml",
+			};
 
-    public static POSDocument[] documents;
-    static {
-        documents = new POSDocument[filenames.length];
-        for (int i = 0; i < filenames.length; i++) {
-            try {
-				documents[i] = POSDocument.parseDocument(new FileInputStream(filenames[i]));
-			} catch (FileNotFoundException e) {
-				throw new RuntimeException("Cannot read "+filenames[i]);
-			}
-        }
-    };
+		    documents = new POSDocument[filenames.length];
+	        for (int i = 0; i < filenames.length; i++) {
+	            try {
+					documents[i] = POSDocument.parseDocument(new FileInputStream(filenames[i]));
+				} catch (FileNotFoundException e) {
+					throw new RuntimeException("Cannot read "+filenames[i]);
+				}
+	        }
+		}
+	}
 
-	private static void parseAndWrite(String filename) throws ParsingException, ValidityException,
+	private void parseAndWrite(String filename) throws ParsingException, ValidityException,
 			IOException, FileNotFoundException {
 		POSDocument document = POSDocument.parseDocument(filename);
 		File infile = new File(filename);
@@ -59,6 +63,32 @@ public class DocumentTest {
 		File outputFile = new File(outputDir, infile.getName());
 		FileOutputStream fos = new FileOutputStream(outputFile);
 		CMLUtil.debug(document, fos, 4);
+	}
+	
+    
+    public List<POSElement> getDocumentList() {
+    	List<POSElement> documentList = new ArrayList<POSElement>();
+    	for (POSElement document : documents) {
+    		documentList.add((POSDocument)document);
+    	}
+    	return documentList;
+    }
+    
+    public List<POSElement> getSentenceList() {
+    	List<POSElement> sentenceList = new ArrayList<POSElement>();
+    	for (POSElement document : documents) {
+    		List<POSElement> sentenceListx = ((POSDocument)document).getSentenceList();
+    		for (POSElement sentence : sentenceListx) {
+    			sentenceList.add((POSSentence)sentence);
+    		}
+    	}
+    	return sentenceList;
+    }
+
+
+	@Before
+	public void setUp() {
+		makeFiles();
 	}
 
     @Test
@@ -81,23 +111,5 @@ public class DocumentTest {
 	    }
     }
     
-    public static List<POSElement> getDocumentList() {
-    	List<POSElement> documentList = new ArrayList<POSElement>();
-    	for (POSElement document : documents) {
-    		documentList.add((POSDocument)document);
-    	}
-    	return documentList;
-    }
-    
-    public static List<POSElement> getSentenceList() {
-    	List<POSElement> sentenceList = new ArrayList<POSElement>();
-    	for (POSElement document : documents) {
-    		List<POSElement> sentenceListx = ((POSDocument)document).getSentenceList();
-    		for (POSElement sentence : sentenceListx) {
-    			sentenceList.add((POSSentence)sentence);
-    		}
-    	}
-    	return sentenceList;
-    }
 
 }
